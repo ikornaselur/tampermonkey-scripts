5fef364bbd977142fe632d3de54f7d3c548fd497
Axel Örn <axelorn@gagnavarslan.is>
2015-05-04 13:29:31 +0000

    Group buttons and reindent to two spaces

--- wanikani.com/Hide WaniKani answers.user.js
+++ wanikani.com/Hide WaniKani answers.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hide WaniKani answers 
 // @namespace    http://absalon.is
-// @version      0.2
+// @version      0.2.1
 // @description  Add a simple button to WaniKani level item list to hide naswers 
 // @author       Axel Örn Sigurðsson
 // @match        http://www.wanikani.com/level/*
@@ -10,76 +10,83 @@
 // ==/UserScript==
 
 (function($) {
-    $(document).ready(function () {
-        /**
-         * Helper functions
-         */
-        $.fn.invisible = function() {
-            if (!answersShown) {
-                return;
-            }
-            return this.each(function() {
-                $(this).css({opacity: 1.0, visibility: "visible"}).animate({opacity: 0.0}, 150);
-            });
-        };
-        $.fn.visible = function() {
-            return this.each(function() {
-                $(this).css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1.0}, 150);
-            });
-        };
-        $.fn.shuffle = function(childElem) {
-            return this.each(function() {
-                var $this = $(this);
-                var elems = $this.children(childElem);
+  $(document).ready(function () {
+    /**
+     * Helper functions
+     */
+    $.fn.invisible = function() {
+      if (!answersShown) {
+        return;
+      }
+      return this.each(function() {
+        $(this).css({opacity: 1.0, visibility: "visible"}).animate({opacity: 0.0}, 150);
+      });
+    };
+    $.fn.visible = function() {
+      return this.each(function() {
+        $(this).css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1.0}, 150);
+      });
+    };
+    $.fn.shuffle = function(childElem) {
+      return this.each(function() {
+        var $this = $(this);
+        var elems = $this.children(childElem);
 
-                elems.sort(function() { return (Math.round(Math.random())-0.5); });
+        elems.sort(function() { return (Math.round(Math.random())-0.5); });  
 
-                $this.detach(childElem);
+        $this.detach(childElem);  
 
-                for(var i=0; i < elems.length; i++)
-                    $this.append(elems[i]);
+        for(var i=0; i < elems.length; i++)
+        $this.append(elems[i]);      
 
-            });
-        };
-        /**
-         * Toggle answers button
-         */
-        var answersShown = false;
-        var answersButton = $('<button class="btn" style="margin: 0 0 10px 20px;">Toggle answers</button>');
-        answersButton.on('click', function () {
-            answersShown = !answersShown;
-            if (!answersShown) {
-                $('.character-item > a > ul').visible();
-            }
-            else {
-                $('.character-item > a > ul').invisible();
-            }
-        });
-        answersButton.insertAfter('.page-list');
-        $('.character-item').hover(function() {
-            if (answersShown) {
-                $('a > ul', this).visible();
-            }
-        }, function() {
-            if (answersShown) {
-                $('a > ul', this).invisible();
-            }
-        });
+      });    
+    };
 
-        /**
-         * Shuffle cards
-         */
-        var shuffleButton = $('<Button class="btn" style="margin: 0 0 10px 20px;">Shuffle answers</button>');
-        var sections = $('section[id^=level-]');
-        shuffleButton.on('click', function () {
-            sections.each(function (_, el) {
-                $('ul', el).shuffle('li');
-            });
-        });
-        shuffleButton.insertAfter('.page-list');
+    /**
+     * Button group for all the buttons
+     */
+    var buttonGroup = $('<div class="btn-group" style="margin: 0 0 10px 20px;"></div>');
+    buttonGroup.insertAfter('.page-list');
 
-        /**
-         * Hide not learned
-         */
+    /**
+     * Toggle answers button
+     */
+    var answersShown = false;
+    var answersButton = $('<button class="btn">Toggle answers</button>');
+    answersButton.on('click', function () {
+      answersShown = !answersShown;
+      if (!answersShown) {
+        $('.character-item > a > ul').visible();
+      }
+      else {
+        $('.character-item > a > ul').invisible();
+      }
     });
+    answersButton.appendTo(buttonGroup);
+    $('.character-item').hover(function() {
+      if (answersShown) {
+        $('a > ul', this).visible();
+      }
+    }, function() {
+      if (answersShown) {
+        $('a > ul', this).invisible();
+      }
+    });
+
+    /**
+     * Shuffle cards
+     */
+    var shuffleButton = $('<Button class="btn">Shuffle answers</button>');
+    var sections = $('section[id^=level-]')
+    shuffleButton.on('click', function () {
+      sections.each(function (_, el) {
+        $('ul', el).shuffle('li');
+      });
+    });
+    shuffleButton.appendTo(buttonGroup);
+
+    /** 
+     * Hide not learned
+     */
+  });
 }(jQuery));
