6ea27956f3d65d0e093ed13ea9597e933806f0e8
Axel Örn <axelorn@gagnavarslan.is>
2015-05-13 11:08:26 +0000

    Add a github milestones button

---
+++ github.com/Github Milestones button.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Github Milestones button
+// @namespace    http://absalon.is
+// @version      0.1
+// @description  Add a 'Milestones' button to a github repo
+// @author       Axel Örn Sigurðsson
+// @match        https://github.com/*/*
+// @grant        none
+// ==/UserScript==
+
+var pulls = $('li[aria-label="Pull requests"]');
+var milestones = $(pulls.clone());
+milestones.attr('aria-label', 'Milestones');
+var href = milestones.children('a').attr('href');
+milestones.children('a').attr('href', href.substring(0, href.lastIndexOf('/')) + '/milestones');
+milestones.children('a').children('.octicon-git-pull-request').removeClass('octicon-git-pull-request').addClass('octicon-milestone');
+milestones.children('a').children('.full-word').text('Milestones');
+milestones.children('a').children('.js-pull-replace-counter').remove();
+milestones.insertAfter(pulls);
