a66295a3c5a2ad62ec0d4ca4238e00596c156642
Axel Orn <axel@fenrir.absalon.is>
2015-05-13 11:12:23 +0000

    Remove selected class on milestones button

--- github.com/Github Milestones button.user.js
+++ github.com/Github Milestones button.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Github Milestones button
 // @namespace    http://absalon.is
-// @version      0.1
+// @version      0.2
 // @description  Add a 'Milestones' button to a github repo
 // @author       Axel Örn Sigurðsson
 // @match        https://github.com/*/*
@@ -16,4 +16,6 @@ milestones.children('a').attr('href', href.substring(0, href.lastIndexOf('/')) +
 milestones.children('a').children('.octicon-git-pull-request').removeClass('octicon-git-pull-request').addClass('octicon-milestone');
 milestones.children('a').children('.full-word').text('Milestones');
 milestones.children('a').children('.js-pull-replace-counter').remove();
+// Remove selected class on the button if the issues button was selected during copy
+milestones.children('a').removeClass('selected');
 milestones.insertAfter(pulls);
